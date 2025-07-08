92c24626d9211ffb416b3dda0709027fed426132
MarcusB86 <marcusb08@gmail.com>
2025-07-08 12:39:10 -0400

    removed MapPin import from home

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { MapPin, Clock, Users, TrendingUp } from 'lucide-react';
+import { Clock, Users, TrendingUp } from 'lucide-react';
 
 const Home = () => {
   return (
